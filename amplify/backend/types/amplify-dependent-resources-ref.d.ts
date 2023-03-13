b42429f6c6981b215a4f3522f1c39c0c8b4e936b
70972627 <18kwd1@queensu.ca>
2023-03-13 16:42:44 -0400

    Initial intergration with api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -57,6 +57,13 @@ export type AmplifyDependentResourcesAttributes = {
       "LambdaExecutionRoleArn": "string",
       "Name": "string",
       "Region": "string"
+    },
+    "ghgViewResultSingle": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   }
 }
--- src/pages/View-Result/ViewResultSingle.js
+++ src/pages/View-Result/ViewResultSingle.js
@@ -23,6 +23,8 @@ import {
 
   ChartJS.register(ArcElement, Tooltip, Legend);
 
+  //const [Data, setData] = useState("")
+
   export const options = {
     plugins: {
       title: {
@@ -100,24 +102,24 @@ import {
 
 function ViewResultSingle(){
 
-    /*async function callAPI() {
-        const user = await Auth.currentAuthenticatedUser()
-        const token = user.signInUserSession.idToken.jwtToken
-        console.log({ token })
-        const requestInfo = {
-          headers: {
-            Authorization: token
-          },
-          queryStringParameters: { 
-            id: 'e36bc896-8a81-4ce1-abf0-f2779f558e5a'
-          }
-        };
-        await API.get('api4ef6c8be', '/ghgViewResult', requestInfo).then((response) => {
-          setData(response)
-          console.log(response)
-        })
-      }
-    */
+    // async function callAPI() {
+    //     const user = await Auth.currentAuthenticatedUser()
+    //     const token = user.signInUserSession.idToken.jwtToken
+    //     console.log({ token })
+    //     const requestInfo = {
+    //       headers: {
+    //         Authorization: token
+    //       },
+    //       queryStringParameters: { 
+    //         id: '31'
+    //       }
+    //     };
+    //     await API.get('api4ef6c8be', '/ghgViewResultSingle', requestInfo).then((response) => {
+    //       setData(response)
+    //       console.log(response)
+    //     })
+    //   }
+    
 
 
       
